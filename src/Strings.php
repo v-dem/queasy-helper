f175275c5163acf90d9d73ccbff3d831c0f7f464
v-dem <vitaly_demyanenko@yahoo.com>
2020-01-31 16:25:13 +0200

    added functions to detect and convert utf8/16/32 encoded text

--- src/Strings.php
+++ src/Strings.php
@@ -4,6 +4,14 @@ namespace queasy\helper;
 
 class Strings
 {
+    private static $ENCODING_BOMS = array(
+        "\x00\x00\xFE\xFF" => 'UTF-32BE',
+        "\xFF\xFE\x00\x00" => 'UTF-32LE',
+        "\xFE\xFF" => 'UTF-16BE',
+        "\xFF\xFE" => 'UTF-16LE',
+        "\xEF\xBB\xBF" => 'UTF-8'
+    );
+
     public static function startsWith($string, $start)
     {
         return strpos($string, $start) === 0;
@@ -12,6 +20,27 @@ class Strings
     public static function endsWith($string, $end)
     {
         return strpos($string, $end) == strlen($string) - strlen($end);
+    }
+
+    public static function encoding($text)
+    {
+        $result = mb_detect_encoding($text);
+        if (false === $result) {
+            foreach (static::$ENCODING_BOMS as $bom => $encoding) {
+                if (self::startsWith($text, $bom)) {
+                    $result = $encoding;
+
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public static function encode($text, $encoding = 'UTF-8')
+    {
+        return mb_convert_encoding($text, $encoding, self::encoding($text));
     }
 }
 
